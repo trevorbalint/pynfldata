d675a6c44f257f131545179206e08e0781eb8859
czarstark <trevor.balint@gmail.com>
2019-09-19 20:38:54 +0900

    coaches_parser - corrected function call and added simple docs

--- pynfldata/coaches_data/coaches_parser.py
+++ pynfldata/coaches_data/coaches_parser.py
@@ -1,4 +1,4 @@
-from pynfldata import nfl_data_parser as f
+from pynfldata.nfl_data_parser import functions as f
 import xmltodict
 import pandas as pd
 import logging
@@ -17,6 +17,7 @@ TEAMS_FILENAME = 'teams.csv'
 COACHES_FILENAME = 'coaches.csv'
 
 
+# fetch the list of teams for a given year
 def get_teams_json(team_year: int):
     logger.debug('Getting team data for {}'.format(str(team_year)))
     url = "http://www.nfl.com/feeds-rs/teams/{year}.xml".format(year=team_year)
@@ -26,6 +27,7 @@ def get_teams_json(team_year: int):
     return data_dict['teamsFeed']['teams']['team']
 
 
+# fetch coach info for a given team/year
 def get_coaches_json(team_year: int, team_id: int):
     logger.debug('Getting coach info for {team:04}, {year:04}'.format(team=team_id, year=team_year))
     url = "http://www.nfl.com/feeds-rs/coach/byTeam/{team:04}/{year:04}".format(team=team_id, year=team_year)
@@ -35,12 +37,16 @@ def get_coaches_json(team_year: int, team_id: int):
     return [data_dict['coach']]
 
 
+# primary function to get and save basic raw teams data
 def save_teams_df():
+    # for each year, get the list of teams for that year
     teams_list = []
-    for year in range(1969, 2019):
+    for year in range(1969, 2020):
         teams_list += get_teams_json(year)
 
     df = pd.DataFrame().from_dict(teams_list)
+
+    # clean the data
     bad_team_abbrs = ['AFE', 'AFW', 'NFE', 'NFW']  # all-pro teams should be excluded
     df = df[df['@teamType'] != 'PRO']
     df = df[~df['@abbr'].isin(bad_team_abbrs)][['@season', '@teamId', '@abbr']]
@@ -51,6 +57,7 @@ def save_teams_df():
     df.to_csv(TEAMS_FILENAME)
 
 
+# function to read the teams data if it exists and generate it if not
 def get_teams_data():
     if not os.path.isfile(TEAMS_FILENAME):
         save_teams_df()
@@ -58,10 +65,11 @@ def get_teams_data():
     return pd.read_csv(TEAMS_FILENAME)
 
 
+# function that reads the team data and gets the coach data for every team/year
 def save_coaches_df():
     teams_df = get_teams_data().set_index(['season', 'teamId'])
 
-    team_list = [x[0] for x in teams_df.iterrows()]
+    team_list = [x[0] for x in teams_df.iterrows()]  # every team/year combo
 
     for team_tuple in team_list:
         raw_coach_data = get_coaches_json(team_tuple[0], team_tuple[1])
@@ -74,6 +82,7 @@ def save_coaches_df():
     teams_df.to_csv(COACHES_FILENAME)
 
 
+# function to read the coaches data if it exists and generate it if not
 def get_coaches_data():
     if not os.path.isfile(COACHES_FILENAME):
         save_coaches_df()
