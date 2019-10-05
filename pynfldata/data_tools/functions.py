3ca298e32609a2d6d1753bdabbcc694e426920cc
czarstark <trevor.balint@gmail.com>
2019-10-05 17:51:59 +0900

    Added get_data function to helper functions
    This function checks to see if the json has already been downloaded
    If it has, use it. If not, get it and save it for next time.

--- pynfldata/data_tools/functions.py
+++ pynfldata/data_tools/functions.py
@@ -2,6 +2,9 @@ import urllib3
 import time
 import xmltodict
 from pynfldata.data_tools.nfl_types import Game
+import json
+from pathlib import Path
+import os
 
 
 # helper function to get the xml and ensure that status 200 is returned
@@ -11,6 +14,32 @@ def download_xml(path: str, timeout_secs: int = 2):
     assert r.status == 200
     time.sleep(timeout_secs)
     return r.data
+
+
+def get_data(path: str, timeout_secs: int = 2, xml_args: dict = dict):
+    # gets the type of data requested, coaches, teams, boxscorepbp, etc, and the rest of the path
+    short_path = path.split('feeds-rs/')[1].split('/')
+
+    # split short_path into folder (nested under /data) and file_path (everything but .xml because we'll be saving json)
+    folder = 'data/{}'.format(short_path[0])
+    file_path = '{}.json'.format(short_path[1].split('.')[0])
+
+    # if the folder doesn't exist, create it
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    # build a Path object and check if it's a file. if it is, use it. If not, download and convert the xml.
+    filename = Path('{}//{}'.format(folder, file_path))
+    print(filename)
+    if filename.is_file():
+        with open(filename, 'r') as infile:
+            json_data = json.load(infile)
+    else:
+        xml_string = download_xml(path, timeout_secs)
+        json_data = xmltodict.parse(xml_string, **xml_args)
+        with open(filename, 'w') as outfile:
+            json.dump(json_data, outfile)
+    return json_data
 
 
 # function to get all games from a schedule file and build Game objects
